03e52a29ebe2a4cdb0d3769f935ae7bde44ffd03
Michael Stelly <mjstelly@gmail.com>
2020-01-04 11:40:18 -0600

    added reactotron and formik

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+// import Reactotron from 'reactotron-react-native';
 
+// import './ReactotronConfig';
+
+if (__DEV__) {
+  import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
+}
 AppRegistry.registerComponent(appName, () => App);
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,27 @@
+// Formik x React Native example
 import React from 'react';
-import {Text, View, TextInput, Button, Alert} from 'react-native';
-import {useForm, Controller} from 'react-hook-form';
+import {Button, TextInput, Text, View} from 'react-native';
+import {Formik} from 'formik';
+import Reactotron from 'reactotron-react-native';
 
-export default function App() {
-  const {control, handleSubmit, errors} = useForm();
-  const onSubmit = data => Alert.alert('Form Data', data);
-
-  return (
-    <View>
-      <Text>First name</Text>
-      <Controller
-        as={<TextInput />}
-        control={control}
-        name="firstName"
-        rules={{required: true}}
-      />
-      {errors.firstName && <Text>This is required.</Text>}
-
-      <Text>Last name</Text>
-      <Controller as={<TextInput />} control={control} name="lastName" />
-
-      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
-    </View>
-  );
-}
+const Login = props => (
+  <Formik
+    initialValues={{email: ''}}
+    onSubmit={values => Reactotron.log(values)}>
+    {({handleChange, handleBlur, handleSubmit, values}) => (
+      // eslint-disable-next-line react-native/no-inline-styles
+      <View style={{flex: 1}}>
+        {Reactotron.log('hello rendering world')}
+        <Text style={{fontSize: 20}}>Email</Text>
+        <TextInput
+          onChangeText={handleChange('email')}
+          onBlur={handleBlur('email')}
+          value={values.email}
+          style={{backgroundColor: 'cyan', height: 30}}
+        />
+        <Button onPress={handleSubmit} title="Submit" />
+      </View>
+    )}
+  </Formik>
+);
+export default Login;
